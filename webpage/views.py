a3c72d27fbcbca6f804918cb4bc56a51f878da12
Codeyg12 <codey.gallup@gmail.com>
2023-07-09 13:06:25 -0600

    aadding all starter paths

--- webpage/views.py
+++ webpage/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html', {})
+
+def add(request):
+    return render(request, 'add.html', {})
+
+def view(request):
+    return render(request, 'view.html', {})
