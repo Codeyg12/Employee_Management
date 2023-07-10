49a08b5ee67fe63415bd2efc879acd8daa39cc26
Codeyg12 <codey.gallup@gmail.com>
2023-07-10 17:19:46 -0600

    starting all models

--- webpage/models.py
+++ webpage/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    department_name = models.CharField(max_length=50)
+
+class Role(models.Model):
+    title = models.CharField(max_length=50)
+    salary = models.IntegerField()
+    role_department = models.ForeignKey(Department)
+
+class Employee(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    employee_role = models.ForeignKey(Role)
+    employee_department = models.ForeignKey(Department)
+    manager = models.ForeignKey('self')
