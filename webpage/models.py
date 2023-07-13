82aa561f77a20b34e46bf6dbe7965ab87a6d5ad3
Codeyg12 <codey.gallup@gmail.com>
2023-07-13 15:18:10 -0600

    changed timeout to 3 seconds, adding str methods to models

--- webpage/models.py
+++ webpage/models.py
@@ -4,10 +4,16 @@ from django.db import models
 class Department(models.Model):
     department_name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return(f'{self.department_name}')
+
 class Role(models.Model):
     title = models.CharField(max_length=50)
     salary = models.IntegerField()
     role_department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return(f'{self.title} in {self.role_department} department')
 
 class Employee(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -16,3 +22,6 @@ class Employee(models.Model):
     employee_role = models.ForeignKey(Role, on_delete=models.CASCADE)
     employee_department = models.ForeignKey(Department, on_delete=models.CASCADE)
     manager = models.ForeignKey('self', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return(f'{self.first_name} {self.last_name} in the role of {self.employee_role}')
