41b4bf0f8432a882727d1c35f9fe5fdb38b77828
Codeyg12 <codey.gallup@gmail.com>
2023-07-26 21:30:22 -0600

    spelling errors

--- employee_management/wsgi.py
+++ employee_management/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for employee_managment project.
+WSGI config for employee_management project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'employee_managment.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'employee_management.settings')
 
 application = get_wsgi_application()
